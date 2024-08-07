13f8730eccf1e9c38e63453c164c0713a0a22184
Sygnom1215 <peacepark112@gmail.com>
2024-08-07 15:54:22 +0900

    Created Header and Footer Structure

--- README.md
+++
@@ -1,1 +0,0 @@
-# front-portfolio-24
---
+++ my-app/src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Footer/Footer"
+import Header from "./Header/Header"
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+        <main>
+            <h1>ThisisLayout</h1>
+        </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
